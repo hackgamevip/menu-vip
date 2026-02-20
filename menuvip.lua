2f825b580be15b11549e9ac475a65b082a301586
hackgamevip <nguyendoconghau2k6@gmail.com>
2026-02-20 14:17:28 +0700

    Update menuvip.lua

--- menuvip.lua
+++ menuvip.lua
@@ -303,7 +303,7 @@ createToggle(page2, "🕹️ FPS Boost (Giảm Lag)", function(v)
         Lighting.GlobalShadows = true; pcall(function() settings().Rendering.QualityLevel = Enum.QualityLevel.Automatic end) 
     end
 end)
-createToggle(page2, "🔴 ESP người chơi", function(v) State.ESP = v end)
+createToggle(page2, "👀 ESP người chơi", function(v) State.ESP = v end)
 createToggle(page2, "💡 Ánh sáng quanh người", function(v) 
     State.PlayerLight = v 
     if not v and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then local light = player.Character.HumanoidRootPart:FindFirstChild("PlayerPointLight"); if light then light:Destroy() end end
@@ -318,7 +318,7 @@ createToggle(page3, "🐿️ Lấy đồ nhanh", function(v)
 end)
 ProximityPromptService.PromptButtonHoldBegan:Connect(function(prompt) if State.Instant then pcall(function() fireproximityprompt(prompt) end) end end)
 createToggle(page3, "🛡️ Chống bị kick (Anti-AFK)", function(v) State.AntiAfk = v end)
-createButton(page3, "💻 BẬT ADMIN (Infinite Yield)", Theme.AccentOn, function() pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() end) end)
+createButton(page3, "💻 Lệnh admin (Infinite Yield)", Theme.AccentOn, function() pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() end) end)
 createButton(page3, "🔨 LẤY BTOOLS", Theme.Brand, function() pcall(function() loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))() end) end)
 createButton(page3, "🕊️ FLY (Bay)", Theme.Brand, function() pcall(function() loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")() end) end)
 createButton(page3, "📂 MENU TP SAVE V2", Theme.Brand, function() pcall(function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/Tp%20Place%20GUI'),true))() end) end)
@@ -348,7 +348,7 @@ local function createPosItem(name, cframe)
 end
 createButton(page4, "🎯 LƯU TỌA ĐỘ ĐANG ĐỨNG", Theme.AccentOn, function()
     if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        savedLocCount = savedLocCount + 1
+        savedLocCount = savedLocCount + 1 (đổi tên)
         createPosItem("📌 Vị trí " .. savedLocCount, player.Character.HumanoidRootPart.CFrame)
     end
 end)
